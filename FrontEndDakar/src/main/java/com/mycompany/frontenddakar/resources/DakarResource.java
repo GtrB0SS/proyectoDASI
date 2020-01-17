cc17c56eef21f47c1a3d8bbd18ea8e8883860d3c
Alberto <alberto-8698@gmail.com>
2020-01-17 17:19:41 +0100

    iuh

---
+++ FrontEndDakar/src/main/java/com/mycompany/frontenddakar/resources/DakarResource.java
@@ -0,0 +1,20 @@
+package com.mycompany.frontenddakar.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("dakar")
+public class DakarResource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping")
+                .build();
+    }
+}
